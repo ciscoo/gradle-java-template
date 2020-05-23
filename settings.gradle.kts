1c05796eb5317e1a9092f61da784ab5e7c3a9bb6
ciscoo <cisco21c@gmail.com>
2020-05-23 11:13:50 -0500

    Enforce build file conventions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,9 @@ pluginManagement {
 }
 
 rootProject.name = "gradle-java-template"
+
+// Enforce build file uses Kotlin DSL and name is the project name
+rootProject.children.forEach { with(it) {
+    buildFileName = "$name.gradle.kts"
+    require(buildFile.isFile) { "$buildFile must exist" }
+} }
