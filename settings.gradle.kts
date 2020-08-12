4fba6af04ae994e3521b9f7dcdc769a764700519
ciscoo <cisco21c@gmail.com>
2020-08-11 22:05:33 -0500

    Polish

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,9 @@ pluginManagement {
 rootProject.name = "gradle-java-template"
 
 // Enforce build file uses Kotlin DSL and name is the project name
-rootProject.children.forEach { with(it) {
-    buildFileName = "$name.gradle.kts"
-    require(buildFile.isFile) { "$buildFile must exist" }
-} }
+rootProject.children.forEach {
+    with(it) {
+        buildFileName = "$name.gradle.kts"
+        require(buildFile.isFile) { "$buildFile must exist" }
+    }
+}
