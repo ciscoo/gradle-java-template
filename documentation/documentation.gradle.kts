436eaed76fd8cb840b4b89936a0e7a57ad9d0a77
ciscoo <cisco21c@gmail.com>
2024-01-16 09:02:17 -0600

    Add metadata as input for tasks

--- documentation/documentation.gradle.kts
+++ documentation/documentation.gradle.kts
@@ -16,13 +16,16 @@ node {
 tasks {
     val generateGradleProjectMetadata by registering(GenerateGradleProjectMetadata::class)
     val vitePressDev by registering(NpmTask::class) {
+        inputs.files(generateGradleProjectMetadata)
         args = listOf("run", "docs:dev")
     }
     val vitePressBuild by registering(NpmTask::class) {
+        inputs.files(generateGradleProjectMetadata)
         args = listOf("run", "docs:build")
         outputs.dir(layout.buildDirectory.dir("vitepress-dist"))
     }
     val vitePressPreview by registering(NpmTask::class) {
+        inputs.files(generateGradleProjectMetadata)
         args = listOf("run", "docs:preview")
     }
 }
