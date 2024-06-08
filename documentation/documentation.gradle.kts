7d4e4df14a05e13e5b5c269d0b7a2d5f530db819
ciscoo <cisco21c@gmail.com>
2024-06-08 14:29:47 -0500

    Nuke VitePress
    Primarily because it does not support multi tags

--- documentation/documentation.gradle.kts
+++ documentation/documentation.gradle.kts
@@ -1,4 +1,3 @@
-import com.github.gradle.node.npm.task.NpmTask
 import io.mateo.build.task.GenerateGradleProjectMetadata
 
 plugins {
@@ -49,61 +48,7 @@ tasks {
     npmInstall {
         outputs.dir(layout.projectDirectory.dir("node_modules"))
     }
-    val vitePressDev by registering(NpmTask::class) {
-        description = "Start VitePress dev server."
-        dependsOn(npmInstall)
-        inputs.files(generateGradleProjectMetadata)
-        args = listOf("run", "docs:dev")
-    }
-    val vitePressBuild by registering(NpmTask::class) {
-        description = "Build the VitePress site for production."
-        dependsOn(npmInstall)
-        inputs.files(generateGradleProjectMetadata)
-        args = listOf("run", "docs:build")
-        outputs.dir(layout.buildDirectory.dir("vitepress-dist"))
-        outputs.upToDateWhen {
-            // Force rerun always since the base URL needs
-            // to contain the correct version in order for
-            // navigation to work correctly.
-            false
-        }
-    }
-    val vitePressPreview by registering(NpmTask::class) {
-        description = "Locally preview the production build."
-        dependsOn(npmInstall)
-        inputs.files(generateGradleProjectMetadata)
-        args = listOf("run", "docs:preview")
-    }
-    val prettierCheck by registering(NpmTask::class) {
-        description = "Check if files are formatted."
-        dependsOn(npmInstall)
-        args = listOf("run", "prettier:check")
-    }
-    val prettierWrite by registering(NpmTask::class) {
-        description = "Rewrites all processed files in place."
-        dependsOn(npmInstall)
-        args = listOf("run", "prettier:write")
-    }
-    val prepareVersionedDocs by registering(Copy::class) {
-        description = "Copies the built the VitePress site a versioned folder."
-        from(vitePressBuild)
-        into(docsDir.map { it.dir(docsVersion) })
-    }
-    val createCurrentDocsFolder by registering(Copy::class) {
-        description = "Copies the built the VitePress site to the 'current' docs folder"
-        from(vitePressBuild)
-        into(docsDir.map { it.dir("current") })
-        onlyIf("replaceCurrentDocs property specified") {
-            replaceCurrentDocs
-        }
-    }
     clean {
         delete(npmInstall)
-    }
-    spotlessCheck {
-        dependsOn(prettierCheck)
-    }
-    spotlessApply {
-        finalizedBy(prettierWrite)
     }
 }
