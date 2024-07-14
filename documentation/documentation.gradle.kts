fbec5c2be75c64b475a66689c00f6acaf6e8f20a
ciscoo <cisco21c@gmail.com>
2024-07-14 11:48:01 -0500

    Apply antora to docs build; remove git publish configuration

--- documentation/documentation.gradle.kts
+++ documentation/documentation.gradle.kts
@@ -1,40 +1,12 @@
 plugins {
     base
-    id("com.diffplug.spotless")
-    alias(libs.plugins.node)
-    alias(libs.plugins.gradleGitPublish)
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.anotra)
 }
-
-val snapshot = rootProject.version.toString().contains("SNAPSHOT")
-val replaceCurrentDocs = project.hasProperty("replaceCurrentDocs")
-val docsVersion = if (snapshot) {
-    "current-SNAPSHOT"
-} else if (replaceCurrentDocs) {
-    "current"
-} else {
-    rootProject.version.toString()
-}
-val docsDir = layout.buildDirectory.dir("ghpages-docs")
 
 node {
     version = provider { layout.projectDirectory.file(".nvmrc").asFile.readText().drop(1).trim() }
     download = true
-}
-
-gitPublish {
-    repoUri = "https://github.com/ciscoo/gradle-java-template.git"
-    branch = "gh-pages"
-    contents {
-        from(docsDir)
-        into("docs")
-    }
-    preserve {
-        include("**/*")
-        exclude("docs/$docsVersion/**")
-        if (replaceCurrentDocs) {
-            exclude("docs/current/**")
-        }
-    }
 }
 
 tasks {
