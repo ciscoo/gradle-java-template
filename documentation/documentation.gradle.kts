e9bd1f250cc3f5d7994d655ffc7d3e80b61c89ae
ciscoo <cisco21c@gmail.com>
2024-08-03 09:38:26 -0500

    Purge Antora

--- documentation/documentation.gradle.kts
+++ documentation/documentation.gradle.kts
@@ -1,23 +1,11 @@
 plugins {
     base
     alias(libs.plugins.spotless)
-    alias(libs.plugins.anotra)
 }
 
 node {
     version = provider { layout.projectDirectory.file(".nvmrc").asFile.readText().drop(1).trim() }
     download = true
-}
-
-antora {
-    setOptions(mapOf(
-        "attributes" to mapOf(
-            "release-version" to version,
-            "release-date" to java.time.LocalDate.now(),
-            "gradle-version" to GradleVersion.current()
-        )
-    ))
-
 }
 
 tasks {
