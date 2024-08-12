a8b797d3ce7b128afb49097e743eb80f00bd7f69
ciscoo <cisco21c@gmail.com>
2024-08-11 22:12:20 -0500

    Define toolchain conventions

---
+++ buildSrc/src/main/kotlin/io.mateo.build.java-toolchain-conventions.gradle.kts
@@ -0,0 +1,41 @@
+import io.mateo.build.JavaToolchainExtension
+
+plugins {
+    id("java") apply false
+}
+
+val extension = extensions.create("javaToolchainExtension", JavaToolchainExtension::class.java)
+val defaultLanguageVersion = extension.targetVersion
+val javaLanguageVersion =
+    providers
+        .gradleProperty("javaLanguageVersion")
+        .map { JavaLanguageVersion.of(it) }
+        .orElse(defaultLanguageVersion)
+val jvmImplementation =
+    providers
+        .gradleProperty("jvmImplementation")
+        .map { it.uppercase(`java.util`.Locale.US) }
+        .map {
+            when (it) {
+                "J9" -> JvmImplementation.J9
+                else -> throw InvalidUserDataException("Unsupported JVM implementation: $it")
+            }
+        }.getOrElse(JvmImplementation.VENDOR_SPECIFIC)
+
+java {
+    toolchain {
+        languageVersion = javaLanguageVersion
+        implementation = jvmImplementation
+    }
+}
+
+tasks {
+    withType<JavaExec>().configureEach {
+        javaLauncher = javaToolchains.launcherFor(java.toolchain)
+    }
+    withType<JavaCompile>().configureEach {
+        outputs.cacheIf("Configured Java language version and default language version match") {
+            javaLanguageVersion.get() == defaultLanguageVersion.get()
+        }
+    }
+}
