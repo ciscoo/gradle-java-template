9fe1e984955e116d4da30b93269609580ec85213
ciscoo <cisco21c@gmail.com>
2024-12-28 11:54:45 -0600

    Add manifest entries

--- buildSrc/src/main/kotlin/io.mateo.build.java-toolchain-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io.mateo.build.java-toolchain-conventions.gradle.kts
@@ -27,6 +27,14 @@ java {
         languageVersion = javaLanguageVersion
         implementation = jvmImplementation
     }
+    manifest {
+        attributes(
+            "Build-Jdk-Spec" to extension.releaseVersion.get().toString(),
+            "Created-By" to "Gradle ${gradle.gradleVersion}",
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version.toString(),
+        )
+    }
 }
 
 tasks {
