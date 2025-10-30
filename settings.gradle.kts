f580f3504b812226859930744226fd974106c5d2
ciscoo <cisco21c@gmail.com>
2025-10-29 21:38:11 -0500

    Remove Spring Milestone repo They are published to Maven Central now

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,21 +16,6 @@ rootProject.name = "gradle-java-template"
 include("documentation")
 include("example-api")
 
-dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-        maven {
-            name = "springMilestone"
-            url = uri("https://repo.spring.io/milestone")
-            content {
-                includeGroupAndSubgroups("org.springframework")
-                includeGroupAndSubgroups("io.projectreactor")
-                includeGroupAndSubgroups("io.micrometer")
-            }
-        }
-    }
-}
-
 fun ProjectDescriptor.ensureBuildFileExists() {
     buildFileName = "$name.gradle.kts"
     require(buildFile.isFile) { "$buildFile must exist" }
