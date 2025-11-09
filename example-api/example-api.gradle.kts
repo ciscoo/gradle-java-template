367517dd4725a37bc66e9eb49b48a9afe4b4e28c
ciscoo <cisco21c@gmail.com>
2025-11-09 09:50:03 -0600

    Fix build

--- example-api/example-api.gradle.kts
+++ example-api/example-api.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("io.mateo.build.java-library-conventions")
-    id("io.mateo.build.maven-publishing-conventions")
+    id("io.mateo.build.publishing-conventions")
 }
 
 description = "Example API"
