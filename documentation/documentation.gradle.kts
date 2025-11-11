f8a3f7f9f3f0ad5e56eb4a00b1e542d89a2f0243
ciscoo <cisco21c@gmail.com>
2025-11-11 16:11:03 -0600

    Configure Node plugin

--- documentation/documentation.gradle.kts
+++ documentation/documentation.gradle.kts
@@ -1,9 +1,19 @@
 plugins {
     id("io.mateo.build.java-conventions")
+    alias(libs.plugins.node)
     alias(libs.plugins.spotless)
 }
 
 description = "Gradle Java Template Documentation"
+
+node {
+    workDir = layout.buildDirectory.dir("node/installation")
+    npmWorkDir = layout.buildDirectory.dir("node/npm")
+    nodeProjectDir = layout.buildDirectory.dir("node/project")
+    if (providers.environmentVariable("CI").isPresent) {
+        npmInstallCommand = "ci --silent --no-progress"
+    }
+}
 
 tasks {
     jar {
