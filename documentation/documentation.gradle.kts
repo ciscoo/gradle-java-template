18bc728d303342a882e22a7d9d4fb16eda7fc1f9
ciscoo <cisco21c@gmail.com>
2025-11-11 21:17:29 -0600

    Configure custom tasks for easier cleaning and outputs

--- documentation/documentation.gradle.kts
+++ documentation/documentation.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.gradle.node.npm.task.NpmTask
+
 plugins {
     id("io.mateo.build.java-conventions")
     alias(libs.plugins.node)
@@ -13,6 +15,29 @@ node {
 }
 
 tasks {
+    val vitePressDev by registering(NpmTask::class) {
+        inputs.files(npmInstall)
+        description = "Start VitePress dev server."
+        npmCommand = listOf("run", "dev")
+        outputs.file(file(".vitepress/cache"))
+    }
+    val vitePressBuild by registering(NpmTask::class) {
+        inputs.files(npmInstall)
+        description = "Build the VitePress site for production."
+        npmCommand = listOf("run", "build")
+        outputs.file(layout.buildDirectory.dir("user-guide"))
+    }
+    clean {
+        delete(vitePressDev)
+        delete(vitePressBuild)
+        delete(
+            npmInstall.map {
+                it.outputs.files.filter { file ->
+                    !file.name.startsWith("package-lock")
+                }
+            },
+        )
+    }
     jar {
         enabled = false
     }
