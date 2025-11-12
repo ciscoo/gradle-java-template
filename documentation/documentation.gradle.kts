ad422f79fde6d4e6e54657abd9eb73bee6242c7b
ciscoo <cisco21c@gmail.com>
2025-11-11 20:24:41 -0600

    Remove config override

--- documentation/documentation.gradle.kts
+++ documentation/documentation.gradle.kts
@@ -7,9 +7,6 @@ plugins {
 description = "Gradle Java Template Documentation"
 
 node {
-    workDir = layout.buildDirectory.dir("node/installation")
-    npmWorkDir = layout.buildDirectory.dir("node/npm")
-    nodeProjectDir = layout.buildDirectory.dir("node/project")
     if (providers.environmentVariable("CI").isPresent) {
         npmInstallCommand = "ci --silent --no-progress"
     }
