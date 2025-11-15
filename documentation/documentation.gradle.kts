41697a7c76e05282cb55aa6926d6b5ec930b0969
ciscoo <cisco21c@gmail.com>
2025-11-14 18:14:21 -0600

    Aggregate javadoc

--- documentation/documentation.gradle.kts
+++ documentation/documentation.gradle.kts
@@ -14,14 +14,57 @@ node {
     }
 }
 
+val aggregateJavadoc by configurations.dependencyScope("aggregateJavadoc")
+
+val javadoc by configurations.dependencyScope("javadoc") {
+    extendsFrom(aggregateJavadoc)
+}
+val javadocClasspath by configurations.resolvable("javadocClasspath") {
+    description = "Classpath for aggregated Javadoc generation."
+    extendsFrom(javadoc)
+    attributes {
+        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_RUNTIME))
+    }
+}
+
+val javadocSource by configurations.dependencyScope("javadocSource") {
+    extendsFrom(aggregateJavadoc)
+}
+val javadocSources by configurations.resolvable("javadocSources") {
+    description = "Java sources for aggregated Javadoc generation."
+    isTransitive = false
+    extendsFrom(javadocSource)
+    attributes {
+        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.VERIFICATION))
+        attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        attribute(VerificationType.VERIFICATION_TYPE_ATTRIBUTE, objects.named(VerificationType.MAIN_SOURCES))
+    }
+}
+
+dependencies {
+    aggregateJavadoc(projects.exampleApi)
+}
+
 tasks {
+    val aggregatedJavadoc by registering(Javadoc::class) {
+        description = "Aggregates Javadoc from projects."
+        classpath = javadocClasspath
+        source(javadocSources.incoming
+            .files
+            .asFileTree.matching { include("**/*.java") }
+        )
+        options {
+            source = javaToolchainExtension.releaseVersion.get().asInt().toString()
+            this as StandardJavadocDocletOptions
     val vitePressDev by registering(NpmTask::class) {
+        inputs.files(aggregatedJavadoc)
         inputs.files(npmInstall)
         description = "Start VitePress dev server."
         npmCommand = listOf("run", "dev")
         outputs.file(file(".vitepress/cache"))
     }
     val vitePressBuild by registering(NpmTask::class) {
+        inputs.files(aggregatedJavadoc)
         inputs.files(npmInstall)
         description = "Build the VitePress site for production."
         npmCommand = listOf("run", "build")
