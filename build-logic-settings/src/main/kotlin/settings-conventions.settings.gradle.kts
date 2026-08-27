ae300dcdc2ac65c9daf1981da5a137a504173df1
ciscoo <cisco21c@gmail.com>
2026-08-27 08:56:04 -0500

    Make accessing projects compatible with isolated projects

--- build-logic-settings/src/main/kotlin/settings-conventions.settings.gradle.kts
+++ build-logic-settings/src/main/kotlin/settings-conventions.settings.gradle.kts
@@ -1,20 +1,3 @@
-import kotlin.io.path.isDirectory
-import kotlin.io.path.listDirectoryEntries
-import kotlin.sequences.forEach
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention")
 }
-
-rootDir
-    .toPath()
-    .listDirectoryEntries()
-    .asSequence()
-    .filter { it.isDirectory() }
-    .map { it.toFile() }
-    .map { it to it.resolve("${it.name}.gradle.kts") }
-    .filter { it.second.exists() }
-    .forEach { (dir, buildScript) ->
-        include(dir.name)
-        project(dir).buildFileName = buildScript.name
-    }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,27 @@ dependencyResolutionManagement {
 
 rootProject.name = "gradle-java-template"
 
+run {
+    val publishedProjectPaths = sortedMapOf<String, String>()
+
+    fun includeProject(projectPath: String, published: Boolean = false) {
+        include(projectPath)
+        project(":$projectPath").apply {
+            buildFileName = "$name.gradle.kts"
+            if (published) {
+                publishedProjectPaths[name] = path
+            }
+        }
+    }
+
+    includeProject("documentation")
+    includeProject("example", published = true)
+
+    gradle.lifecycle.beforeProject {
+        extra["publishedProjectPaths"] = publishedProjectPaths.values.toList()
+    }
+}
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 enableFeaturePreview("NO_IMPLICIT_LOOKUP_IN_PARENT_PROJECTS")
